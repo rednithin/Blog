ee7e6ccb87fc0d45931ac564975fbd8fcd4dc8f0
rednithin <reddy.nithinpg@live.com>
2020-08-16 15:00:55 +0530

    Added github deploy script.

---
+++ github_deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+rm -rf public
+git clone git@github.com:rednithin/rednithin.github.io public
+cd public
+mv .git /tmp/git
+rm -rf *
+cd ..
+yarn run build
+zola build
+cd public
+mv /tmp/git .git
+git add -A
+git commit -m "Finished build"
+git push
+cd ..
